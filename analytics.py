d8e997fb9766e4ed8cab3020a9650e0fcbb802d3
Andrew Hadden <ahadden09@gmail.com>
2018-09-14 09:50:45 -0600

    continued development

--- analytics.py
+++ analytics.py
@@ -51,6 +51,14 @@ pd.set_option('display.max_columns', 10)
 #Create Aggregate Table by player_id and gsis_id (game_id)
 #	e.g. player_id, gsis_id, week, year, fantasy_pts, attempts, etc...
 
+
+primary_stats_all = ['rushing_att','rushing_yds','rushing_tds','receiving_rec','receiving_yds','receiving_tds']
+primary_stats_rush = ['rushing_att','rushing_yds','rushing_tds']
+primary_stats_rec = ['receiving_rec','receiving_yds','receiving_tds']
+all_stats = {'total_yds':{'RB':primary_stats_all,'WR':primary_stats_all}, 'total_tds':{'RB':primary_stats_all,'WR':primary_stats_rec},'first_down':{'RB':primary_stats_all,'WR':primary_stats_rec},'rushing_att':{'RB':primary_stats_rush,'WR':primary_stats_rush},'rushing_yds':{'RB':primary_stats_rush,'WR':primary_stats_rush},'yards_per_carry':{'RB':primary_stats_rush,'WR':primary_stats_rush},'receiving_tar':{'RB':primary_stats_all,'WR':primary_stats_rec},'receiving_rec':{'RB':primary_stats_all,'WR':primary_stats_rec},'receiving_yds':{'RB':primary_stats_all,'WR':primary_stats_rec},'yards_per_tar':{'RB':primary_stats_all,'WR':primary_stats_rec},'yards_per_rec':{'RB':primary_stats_all,'WR':primary_stats_rec},'avg_depth_of_tar':{'RB':primary_stats_all,'WR':primary_stats_rec},'receiving_yac_yds':{'RB':primary_stats_all,'WR':primary_stats_rec}}
+
+
+
 def create_player_agg_total(year, position, week = 0, season_type = 'Regular'):
 	
 	year = (year)
@@ -121,6 +129,7 @@ def create_player_agg_total(year, position, week = 0, season_type = 'Regular'):
 	#player_df['depth_of_rec'] = player_df['receiving_yds'] - player_df['receiving_yac_yds']
 	player_df['total_yds'] = player_df['receiving_yds'] + player_df['rushing_yds']
 	player_df['depth_of_tar'] = player_df['passing_cmp_air_yds'] + player_df['passing_incmp_air_yds']
+	player_df['total_tds'] = player_df['rushing_tds'] + player_df['receiving_tds']
 	
 	if week > 0:
 		player_df = player_df[player_df.week == week]
@@ -133,14 +142,20 @@ def create_player_agg_total(year, position, week = 0, season_type = 'Regular'):
 	agg_df['yards_per_rec'] = agg_df['receiving_yds']/agg_df['receiving_rec']
 	agg_df['avg_depth_of_rec'] = agg_df['depth_of_tar']/agg_df['receiving_rec']
 	
+	
 	return agg_df
 	
 	
-def get_weekly_stat(year, week, position, stat, season = 'regular'):
+def get_weekly_stat(year, week, position, stat, season = 'Regular'):
 	
-	positional_stats = {'RB':[stat,'rushing_att','rushing_yds','rushing_tds','receiving_rec','receiving_yds','receiving_tds'], 'WR':[stat, 'receiving_rec','receiving_yds','receiving_tds']} #stats you always want to see for a given position
+	#I'm thinking I might want to do stats by Call Stat instead of by position
 	
-	columns = [x for x in positional_stats[position]]
+	positional_stats = {'RB':[stat,primary_stats_rush], 'WR':[stat, primary_stats_rec]} #stats you always want to see for a given position
+	
+	#columns = [y for y in positional_stats[position]]
+	columns = [x for x in all_stats[stat][position] if x != stat]
+	
+	columns.insert(0, stat)
 	
 	stat_df = create_player_agg_total(year, position, week, season).sort_values(by=stat, ascending = False)
 
@@ -152,12 +167,10 @@ def get_weekly_stat(year, week, position, stat, season = 'regular'):
 def create_team_agg_total(year, week = 0, season_type = 'Regular'):
 	
 	play_sql = ''' select
-			p.gsis_id,
 			g.week,
 			g.season_year as year,
-			g.season_type,
-			p.drive_id,
 			p.pos_team,
+			case when p.pos_team = g.home_team then g.away_team else g.home_team end as opp_team,
 			1 as play_count,
 			p.first_down,
 			p.passing_first_down,
@@ -181,24 +194,38 @@ def create_team_agg_total(year, week = 0, season_type = 'Regular'):
 	
 	play_df = pd.read_sql(play_sql, db, params = {'type':season_type, 'year':year})
 	
+	#Add new stat columns
 	play_df['total_offense_yds'] = play_df['receiving_yds'] + play_df['rushing_yds']
 	play_df['total_offense_tds'] = play_df['receiving_tds'] + play_df['rushing_tds']
-	
-	#play_df.to_csv('play_df.csv')
-	
+
+	#Limit to week specified, otherwise return season stats
 	if week > 0:
 		play_df = play_df[play_df.week == week]
 	
+	#Group up, sum columns, sort by total_offense_yds
 	agg_play = play_df.groupby(['pos_team','year','week']).aggregate(np.sum).sort_values(by='total_offense_yds', ascending = False)	
 	
-	# order by values
-	# specify columns
+	#Add new agg stat columns
+	agg_play['rush_pct'] = agg_play['rushing_att'] / agg_play['play_count']
+	agg_play['pass_pct'] = agg_play['passing_att'] / agg_play['play_count']
 	
-	return agg_play.head(n=32).style
+	#Define return columns
+	#should I make several options for this?
+	agg_play = agg_play[['total_offense_yds','total_offense_tds','play_count','first_down','rush_pct','rushing_yds','rushing_tds','pass_pct','passing_yds','passing_tds']]
 	
-def get_season_player(year, name, season = 'regular'):	
-	print 'gotsta make this'
+	
+	return agg_play.head(n=32)
+	
+def get_season_player(year, name, season = 'Regular'):	
+	print 'get_season_player - gotsta make this!'
 
+	
+def create_all_analytics(year, week, position, season = 'Regular'):
+	analytics_df = []
+	for stat_type in all_stats:
+		analytics_df.append(get_weekly_stat(year, week, position, stat_type))
+	return analytics_df
+	
 	#Carries
 	#Targets
 	#Receptions	
@@ -211,22 +238,25 @@ def get_season_player(year, name, season = 'regular'):
 	#Yards After Catch
 	#Number First Downs		
 	
-print 'Top Total Yds - RB\n', get_weekly_stat(2018, 3, 'RB', 'total_yds', 'Preseason'), '\n'	
-print 'Top Receptions - RB\n', get_weekly_stat(2018, 3,'RB','receiving_rec','Preseason'), '\n'	
-print 'Top Carries - RB\n', get_weekly_stat(2018, 3, 'RB', 'rushing_att','Preseason'), '\n'
-print 'Top Targets - RB\n', get_weekly_stat(2018, 3, 'RB', 'receiving_tar','Preseason'), '\n'
-print 'Top Rushing Yds - RB\n', get_weekly_stat(2018, 3,'RB','rushing_yds','Preseason'), '\n'
-print 'Top Yds Per Carry - RB\n', get_weekly_stat(2018, 3, 'RB', 'yards_per_carry', 'Preseason'), '\n'
-print 'Top Yds Per Target - RB\n', get_weekly_stat(2018, 3, 'RB', 'yards_per_tar', 'Preseason'), '\n'
-print 'Top Yds Per Reception - RB\n', get_weekly_stat(2018, 3, 'RB', 'yards_per_rec', 'Preseason'), '\n'
-print 'Top Depth of Target - RB\n', get_weekly_stat(2018, 3, 'RB', 'avg_depth_of_tar', 'Preseason'), '\n'
-print 'Top Depth of Reception - RB\n', get_weekly_stat(2018, 3, 'RB', 'avg_depth_of_rec', 'Preseason'), '\n'
-print 'Top First Downs - RB\n', get_weekly_stat(2018, 3, 'RB', 'first_down', 'Preseason'), '\n'
+
+	
+# print 'Top Total Yds - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'total_yds', 'Preseason'), '\n'	
+# print 'Top Receptions - RB\n', analytics.get_weekly_stat(2018, 3,'RB','receiving_rec','Preseason'), '\n'	
+# print 'Top Carries - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'rushing_att','Preseason'), '\n'
+# print 'Top Targets - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'receiving_tar','Preseason'), '\n'
+# print 'Top Rushing Yds - RB\n', analytics.get_weekly_stat(2018, 3,'RB','rushing_yds','Preseason'), '\n'
+# print 'Top Yds Per Carry - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'yards_per_carry', 'Preseason'), '\n'
+# print 'Top Yds Per Target - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'yards_per_tar', 'Preseason'), '\n'
+# print 'Top Yds Per Reception - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'yards_per_rec', 'Preseason'), '\n'
+# print 'Top Depth of Target - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'avg_depth_of_tar', 'Preseason'), '\n'
+# print 'Top Depth of Reception - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'avg_depth_of_rec', 'Preseason'), '\n'
+# print 'Top First Downs - RB\n', analytics.get_weekly_stat(2018, 3, 'RB', 'first_down', 'Preseason'), '\n'
 
 
-print create_team_agg_total(2018, 3, 'Preseason')
+# print analytics.create_team_agg_total(2018, 3, 'Preseason')
 
-get_season_player(2017, 'name')
+# analytics.get_season_player(2017, 'name')
+
 
 
 
--- email_me.py
+++ email_me.py
@@ -1,20 +1,62 @@
 import smtplib
+import pandas as pd
 
-host = 'smtp.gmail.com'
-port = 587
-email = raw_input('email address:')
-password = raw_input('password:')
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-smtp_obj = smtplib.SMTP(host, port) #Create SMTP object
 
-print smtp_obj.ehlo() #Send SMTP "Hello" message		***Will probably need to add a error handling here in case connection fails***
-
-print smtp_obj.starttls() #Start TLS encryption
-
-smtp_obj.login(email, password)
-
-body = 'Subject: NFLdb Daily Digest\nTesting 123'
-
-smtp_obj.sendmail(email, email, body)
-
-smtp_obj.quit()
+def send_email(data, email_to, email_from):
+	host = 'smtp.gmail.com'
+	port = 587
+	login_email = raw_input('login_email address:')
+	password = raw_input('password:')
+	try:
+		smtp_obj = smtplib.SMTP(host, port) #Create SMTP object
+		smtp_obj.ehlo() #Send SMTP "Hello" message		***Will probably need to add a error handling here in case connection fails***
+		smtp_obj.starttls() #Start TLS encryption
+	except:
+		print 'smtp_connection_error: ehlo() or starttls()'
+	try:
+		smtp_obj.login(login_email, password) #Login to email
+	except:
+		print 'stmp_login_error: smtp_login_issue'
+		raise
+	try: #created using code stolen (but not all copy pasta) from https://stackoverflow.com/questions/882712/sending-html-email-using-python	
+		#Create message container - the correct MIME type is multipart/alternative
+		msg = MIMEMultipart('alternative')
+		msg['Subject'] = 'NFLdb Daily Digest'
+		msg['From'] = email_from
+		msg['To'] = email_to
+		
+		#create the body of the message (a plain-test and an HTML version).
+		text = 'This is a plain-text test!/nThis is only a test!'
+		html = '''\
+				<html>
+				  <head></head>
+				  <body>
+					<table>
+						{}
+					</table>
+				  </body>
+				</html>
+				'''.format(data.to_html())
+				
+		#Records the MIME types of both parts - text/plain and text/html
+		part1 = MIMEText(text, 'plain')
+		part2 = MIMEText(html, 'html')
+		
+		# Attach parts into message container.
+		# According to RFC 2046, the last part of a multipart message, in this case
+		# the HTML message, is best and preferred.
+		msg.attach(part1)
+		msg.attach(part2)
+	except:
+		raise
+	try: #Send message via SMTP server
+		body = data
+		smtp_obj.sendmail(email_from, email_to, msg.as_string())
+		print 'email sent'
+		smtp_obj.quit()
+	except:
+		print 'send_email_error: email could not be sent'
+		
